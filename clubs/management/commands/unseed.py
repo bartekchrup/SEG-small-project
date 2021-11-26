4fc8af34c2e4eab5b6250cb6ed71f57dc077269c
K20066796 <k20066796@kcl.ac.uk>
2021-11-26 16:45:56 +0000

    addition of Seed, Unseed and admin.py

--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,6 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
-from clubs.models import User
+from faker import Faker
+from ...models import User
 
 class Command(BaseCommand):
+    def __init__(self):
+        super().__init__()
+        self.faker = Faker('en_GB')
+
     def handle(self, *args, **options):
-        User.objects.filter(is_staff=False, is_superuser=False).delete()
+        print('Deleting all users ')
+        for user in User.objects.all():
+            if not user.is_superuser:
+                user.delete()
